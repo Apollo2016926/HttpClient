c986a63cde7cd59bfe486639533dd9c3c8bc7d8d
gexx <944644308@qq.com>
2019-05-03 22:17:03 +0800

    获取网页图片

--- src/main/java/com/xxxhttpclient/HelloWord5.java
+++ src/main/java/com/xxxhttpclient/HelloWord5.java
@@ -1,11 +1,9 @@
 package com.xxxhttpclient;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -13,13 +11,12 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-public class HelloWord4 {
+public class HelloWord5 {
 	public static void main(String[] args) throws ClientProtocolException,
 			IOException {
 
 		CloseableHttpClient httpClient = HttpClients.createDefault();
-		HttpGet httpGet = new HttpGet(
-				"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556902048853&di=6de782cd5224abc855b78a59ecc8406f&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F0338cbe93580d5e6b0e89f25531541d455f66fda4a6a5-eVWQaf_fw658");
+		HttpGet httpGet = new HttpGet("http://www.tuicool.com");
 		httpGet.setHeader(
 				"User-Agent",
 				"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36");
@@ -27,13 +24,10 @@ public class HelloWord4 {
 		CloseableHttpResponse response = null;
 		response = httpClient.execute(httpGet);
 		HttpEntity entity = response.getEntity();
-		if (entity != null) {
-			System.out.println(entity.getContentType().getValue());
-			InputStream content = entity.getContent();
-			FileUtils.copyToFile(content, new File("D://mienv.jpg"));
-
-		}
-
+		String string = EntityUtils.toString(entity, "utf-8");
+		System.out.println(entity.getContentType().getValue());//获取响应ContentType
+		StatusLine statusLine = response.getStatusLine();//获取响应code
+		System.out.println(statusLine.getStatusCode());
 		response.close();
 		httpClient.close();
 
